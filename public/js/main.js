aeb0728f0502ddb7fc11fb791eb41b2ac3ba2b19
DevDas <epratapdas@gmail.com>
2022-03-28 05:12:31 +0530

    day and date problem solve

--- public/js/main.js
+++ public/js/main.js
@@ -60,3 +60,30 @@ const getInfo = async(event) => {
 }
 
 submitBtn.addEventListener('click', getInfo);
+
+
+
+
+const curDay = document.getElementById("day");
+const getCurrentDay = () => {
+    var weekday = new Array(7);
+    weekday[0] = "Sunday";
+    weekday[1] = "Monday";
+    weekday[2] = "Tuesday";
+    weekday[3] = "Wednesday";
+    weekday[4] = "Thursday";
+    weekday[5] = "Friday";
+    weekday[6] = "Saturday";
+
+    let currentTime = new Date();
+    let day = weekday[currentTime.getDay()];
+    return day;
+  };
+  curDay.innerHTML = getCurrentDay();
+
+//   const curDate = document.getElementById("today_date");
+//   curDate=new Date();
+//   curDate.innerHTML=getDate();
+// document.getElementById('#today_date').innerHTML=currentDate.getDate();
+const d = new Date();
+document.getElementById("today_date").innerHTML = d.getDate() + " - " + (d.getMonth()+1);
